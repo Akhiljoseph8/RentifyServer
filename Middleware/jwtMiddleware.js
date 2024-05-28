a6988fcf3053bd00d99976429f7caaa1442d6466
Akhil Joseph <120910561+Akhiljoseph8@users.noreply.github.com>
2024-05-28 11:36:22 +0530

    Update jwtMiddleware.js

--- Middleware/jwtMiddleware.js
+++ Middleware/jwtMiddleware.js
@@ -4,7 +4,7 @@ const jwtMiddleware = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
         if (token) {
-            const result = jwt.verify(token, process.env.secretkey)
+            const result = jwt.verify(token, secretKey)
             req.payload = result.userId
 
         } else {

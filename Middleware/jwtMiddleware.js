57c7a2b252e86bceed3f7fab2512e5f74c2890c3
Akhil Joseph <120910561+Akhiljoseph8@users.noreply.github.com>
2024-05-28 11:35:53 +0530

    Update jwtMiddleware.js

--- Middleware/jwtMiddleware.js
+++ Middleware/jwtMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const jwtMiddleware = (req, res, next) => {
+     const secretKey="secretkey123"
     try {
         const token = req.headers.authorization.split(" ")[1]
         if (token) {
